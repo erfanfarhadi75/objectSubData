57c5c59b216dc30fc53955f9f6712bbc054dc090
Erfan Farhadi <60686417+erfanfarhadi75@users.noreply.github.com>
2022-07-03 10:47:58 +0430

    remove extra items

--- object-sub-data.pipe.ts
+++ object-sub-data.pipe.ts
@@ -1,13 +1,11 @@
 import {Input, Pipe, PipeTransform} from '@angular/core';
-import {hasValue} from 'ifslib';
+import {hasValue} from 'object.helper';
 
 @Pipe({
   name: 'objectSubData',
 })
 export class ObjectSubDataPipe implements PipeTransform {
   @Input() objectSubData: any;
-
-  // tslint:disable-next-line:no-unnecessary-initializer
   transform(value: any, field: string, defval = undefined): any {
     if (!hasValue(value) || !hasValue(field)) {
       return null;
@@ -17,7 +15,6 @@ export class ObjectSubDataPipe implements PipeTransform {
     if (typeof defval === 'undefined') {
       defval = null;
     }
-    // @ts-ignore
     prop = prop?.split('.');
     for (let i = 0; i < prop.length; i++) {
       if (typeof obj[prop[i]] === 'undefined') {
